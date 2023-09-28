211ba57a69f799467f8220987425f936352af0b2
NEHA MAURYA <nehav3402@gmail.com>
2023-09-29 01:58:18 +0530

    added template

--- Drillo/drillo/urls.py
+++ Drillo/drillo/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('event/', include('eventman.urls')),
