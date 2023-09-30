793eef92bf2b24f36df5fba6a32eaa3fb1eb45d9
Varun Agarwal <104075461+1010varun@users.noreply.github.com>
2023-09-30 10:10:04 +0530

    Revert "added template"
    This reverts commit c01f2600d6b8711685d9f2f5eca663fa20cc3065.

--- Drillo/drillo/urls.py
+++ Drillo/drillo/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('event/', include('eventman.urls')),
