7857c346e295bb8bb1de771ca097eda9d5233375
NEHA MAURYA <nehav3402@gmail.com>
2023-10-30 11:10:12 +0530

    added email template

--- Drillo/recruitment23/admin.py
+++ Drillo/recruitment23/admin.py
@@ -5,6 +5,19 @@ from django.core import mail
 from django.template.loader import render_to_string
 import threading
 from threading import Thread
+from django.conf import settings
+
+class EmailThread(threading.Thread):
+    def __init__(self, subject, html_content, recipient_list):
+        self.subject = subject
+        self.recipient_list = recipient_list
+        self.html_content = html_content
+        threading.Thread.__init__(self)
+
+    def run (self):
+        msg = EmailMessage(self.subject, self.html_content, settings.EMAIL_HOST_USER, bcc=self.recipient_list)
+        msg.content_subtype = "html"
+        msg.send()
 
 def send_html_mail(subject, html_content, recipient_list):
     EmailThread(subject, html_content, recipient_list).start()
@@ -42,10 +55,11 @@ class recruitmentsAdmin(admin.ModelAdmin):
 
         connection.open()
         message= render_to_string('confirmation.html')
-        send_html_mail('Innogeeks Recruitment | Due Payment',message,pl) 
-        send_html_mail('Innogeeks Recruitment | Due Payment',message,pl2)
+        send_html_mail('Innogeeks Recruitment | Registration Successful',message,pl) 
+        send_html_mail('Innogeeks Recruitment | Registration Successful',message,pl2)
 
         connection.close()
+        queryset.update(payment_status=True)
     send_confirmation_mail.short_description = "Send an email for due paymemt"
 
 
